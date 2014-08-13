a0b82877d48ee39724c2d058ad53c14ad490e9ac
Go <gaojie886@qq.com>
2014-08-13 12:55:33 +0800

    支持task、

--- auto/task/materiel/__init__.py
+++ auto/task/materiel/__init__.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 from auto import app
 from common.framework import get_module_package, blueprint_module
+from auto.task import url_prefix as father_url
 
 __author__ = 'GaoJie'
 # 获取所有模块名称
 module_list, package_list = get_module_package(__path__[0])
-blueprint_list, url_prefix = blueprint_module(module_list, __name__)
+
+blueprint_list, url_prefix = blueprint_module(module_list, __name__, father_url)
 
 # 注册蓝图
 for item in blueprint_list.values():
@@ -14,4 +16,3 @@ for item in blueprint_list.values():
 # 加载扩展包
 for pa in package_list:
     __import__('%s.%s' % (__name__, pa))
-
