acf45644613d26e51cbc54fedfdc3d38faa7ea7c
Go <gaojie886@qq.com>
2014-08-13 12:56:50 +0800

    支持task多层级，简化logger获取

--- auto/task/adgroup.py
+++ auto/task/adgroup.py
@@ -2,13 +2,18 @@
 """
 adgroup的脚本任务
 """
+from auto.decorators import action
+from common.decorators import responsed
 from common.framework import get_module_blueprint
-from auto.task import task_blueprint
+from auto.task import blueprint_list
 
 __author__ = 'GaoJie'
-task = get_module_blueprint(task_blueprint, __name__)
+instance = get_module_blueprint(blueprint_list, __name__)
+action_list = []
 
 
-@task.route('/cache')
+@action(action_list)
+@instance.route('/cache')
+@responsed
 def cache():
     print 1
--- auto/task/materiel/iqiyi.py
+++ auto/task/materiel/iqiyi.py
@@ -1,21 +1,25 @@
 # coding=utf-8
 """
-creative的脚本任务
+爱奇艺的脚本任务
 """
+from auto import app
 from auto.decorators import action
 from common.decorators import responsed
 from common.framework import get_module_blueprint, get_current_logger
-from auto.task import blueprint_list
-from auto import app, logger as parent_logger
+from auto.task.materiel import blueprint_list
 
 __author__ = 'GaoJie'
-task = get_module_blueprint(blueprint_list, __name__)
+instance = get_module_blueprint(blueprint_list, __name__)
 logger = get_current_logger(app, __name__)
 action_list = []
 
 
 @action(action_list)
-@task.route('/cache')
+@instance.route('/status')
 @responsed
-def cache():
-    logger.debug(12312312)
+def status():
+    """
+    审核状态判断
+    :return:
+    """
+    logger.debug('iqiyi test')
