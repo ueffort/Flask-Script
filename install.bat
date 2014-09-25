bc3302e384be97c9a9ae7bfbbf8512970fa3f45f
Go <gaojie886@qq.com>
2014-09-25 09:30:39 +0800

    补充依赖

--- install.bat
+++ install.bat
@@ -3,4 +3,5 @@ pip install flask
 pip install flask-mail
 pip install flask-sqlalchemy
 pip install redis
+pip install mysql-python
 deactivate
--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ pip install flask
 pip install flask-mail
 pip install flask-sqlalchemy
 pip install redis
+pip install mysql-python
 deactivate
