a397086b1a58fd42f8610c60306d5cfc25fa4182
Go <gjgj821@gmail.com>
2016-08-16 09:39:31 +0800

    建立全局连接池

--- core/model/__init__.py
+++ core/model/__init__.py
@@ -6,3 +6,19 @@ __author__ = 'GaoJie'
 
 db = SQLAlchemy(session_options={'autocommit': True, 'autoflush': True})
 
+
+def connected(engine):
+    conn_map = {}
+
+    def f(**kwargs):
+        if not kwargs:
+            # 默认参数
+            kwargs = {"autocommit": True}
+        v = ''.join(["%s%s" % (str(k), str(v)) for k, v in kwargs.items()])
+        m5 = hashlib.md5()
+        m5.update(v)
+        key = m5.hexdigest()
+        if key not in conn_map:
+            conn_map[key] = engine.connect().execution_options(**kwargs)
+        return conn_map[key]
+    return f
